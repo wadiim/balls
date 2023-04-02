179fe239134ab0b39e6f47215519f980fd32e19e
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2023-04-02 16:57:44 +0200

    Prepare project structure for stage 1

---
+++ Presentation/View/MainWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace View
+{
+    static class MainWindow
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
