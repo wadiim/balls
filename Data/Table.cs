3f351e875f87abbdabe42a6831c54354c35827e1
MateuszG2001 <mateusz.gladki1236@gmail.com>
2023-04-03 15:55:49 +0200

    Add Table class

---
+++ Data/Table.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    class Table
+    {
+        float width;
+        float height;
+
+        public Table(float width, float height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
