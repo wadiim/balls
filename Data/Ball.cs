9318b5fb98b3dc49342dba4972214791c1377ea2
MateuszG2001 <mateusz.gladki1236@gmail.com>
2023-04-03 15:52:53 +0200

    Add Ball class

---
+++ Data/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace Data
+{
+    class Ball
+    {
+        private static int nextId = 0;
+
+        public int id;
+        public Vector2 position;
+        public float mass;
+        public float radius;
+        public Vector2 velocity;
+
+        public Ball(Vector2 position, Vector2 velocity)
+        {
+            this.position = position;
+            this.velocity = velocity;
+        }
+
+        public void setVelocity(Vector2 newVelocity)
+        {
+
+        }
+
+        public void updatePosition()
+        {
+
+        }
+    }
+}
