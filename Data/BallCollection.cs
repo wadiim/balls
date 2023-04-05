0e7026c4abb50be649f819ab5c671413494acec6
MateuszG2001 <mateusz.gladki1236@gmail.com>
2023-04-05 21:05:40 +0200

    Implement BallCollection class

--- Data/BallCollection.cs
+++ Data/BallCollection.cs
@@ -1,10 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Numerics;
 
 namespace Data
 {
-    class Class1
+    public class BallCollection
     {
+        private readonly List<Ball> Balls;
+        private static readonly Random Rand = new Random();
+
+        public BallCollection()
+        {
+            Balls = new List<Ball>();
+        }
+
+        public void CreateBalls(int NumOfBalls, Vector2 maxPosition, Vector2 maxVelocity)
+        {
+            for (int i = 0; i < NumOfBalls; ++i)
+            {
+                Balls.Add(new Ball(
+                    new Vector2(
+                        (float)Rand.NextDouble() * maxPosition.X,
+                        (float)Rand.NextDouble() * maxPosition.Y
+                        ),
+                    new Vector2(
+                        (float)Rand.NextDouble() * maxVelocity.X,
+                        (float)Rand.NextDouble() * maxVelocity.Y
+                        )
+                    ));
+            }
+        }
+
+        public Ball GetBall(int index)
+        {
+            return Balls[index];
+        }
+
+        public int Count()
+        {
+            return Balls.Count;
+        }
     }
 }
