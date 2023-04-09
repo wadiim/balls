f0e55c4253535d279d447a08be6993bdd44aceac
MateuszG2001 <mateusz.gladki1236@gmail.com>
2023-04-09 11:13:13 +0200

    Implement ViewModel layer

---
+++ Presentation/ViewModel/MainVievModel.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Model;
+
+namespace ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly ModelAbstractAPI modelAPI;
+        public ObservableCollection<IBall> Balls { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand StartButtonClick { get; set; }
+        public int CanvasWidth { get; }
+        public int CanvasHeight { get; }
+        private string _InputText;
+
+        public MainViewModel()
+        {
+            System.Diagnostics.Trace.WriteLine("Initializing MainViewModel");
+
+            modelAPI = ModelAbstractAPI.CreaetModelAPI();
+            Balls = new ObservableCollection<IBall>();
+
+            CanvasWidth = modelAPI.GetCanvasWidth();
+            CanvasHeight = modelAPI.GetCanvasHeight();
+
+            StartButtonClick = new RelayCommand(() => StartButtonClickHandler());
+        }
+
+        private void StartButtonClickHandler()
+        {
+            System.Diagnostics.Trace.WriteLine("Handling start button click");
+
+            int numOfBalls = ReadFromTextBox();
+            modelAPI.StartSimulation(numOfBalls);
+
+            for (int i = 0; i < numOfBalls; i++)
+            {
+                Balls.Add(modelAPI.GetBallModel(i));
+            }
+        }
+
+        public string InputText
+        {
+            get => _InputText;
+            set
+            {
+                _InputText = value;
+                NotifyPropertyChanged(nameof(InputText));
+            }
+        }
+
+        public int ReadFromTextBox()
+        {
+            if (int.TryParse(InputText, out _) && InputText != "0")
+            {
+                int number = int.Parse(InputText);
+                System.Diagnostics.Trace.WriteLine("Number of balls read from input: " + number);
+                return number > 10 ? 10 : number;
+            }
+            return 0;
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Presentation/ViewModel/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        public RelayCommand(Action execute) : this(execute, null) { }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            m_Execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            m_CanExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (m_CanExecute == null)
+            {
+                return true;
+            }
+
+            return parameter == null ? m_CanExecute() : m_CanExecute();
+        }
+
+        public virtual void Execute(object parameter)
+        {
+            m_Execute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        private readonly Action m_Execute;
+        private readonly Func<bool> m_CanExecute;
+    }
+}
