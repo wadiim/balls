1f71ed2bd3cac19aa2f604cd4234a2dbbc0f289e
MateuszG2001 <mateusz.gladki1236@gmail.com>
2023-04-09 11:09:42 +0200

    Implement model layer

---
+++ Presentation/Model/BallModel.cs
@@ -0,0 +1,66 @@
+﻿using System.ComponentModel;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace Model
+{
+    public class BallModel : IBall
+    {
+        public int Id { get; }
+
+        private double _Top;
+        public double Top
+        {
+            get => _Top;
+            set
+            {
+                if (_Top == value)
+                {
+                    return;
+                }
+
+                _Top = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private double _Left;
+        public double Left
+        {
+            get => _Left;
+            set
+            {
+                if (_Left == value)
+                {
+                    return;
+                }
+
+                _Left = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public float Diameter { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BallModel(int id, double top, double left, float diameter)
+        {
+            Id = id;
+            Top = top;
+            Left = left;
+            Diameter = diameter;
+        }
+
+        public void Move(double top, double left)
+        {
+            Top = top;
+            Left = left;
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Presentation/Model/IBall.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Numerics;
+using Logic;
+
+namespace Model
+{
+    public interface IBall : INotifyPropertyChanged
+    {
+        int Id { get; }
+        double Top { get; }
+        double Left { get; }
+        float Diameter { get; }
+    }
+
+    public abstract class ModelAbstractAPI : IObserver<int>, IObservable<IBall>
+    {
+        public abstract void OnCompleted();
+
+        public abstract void OnError(Exception error);
+
+        public abstract void OnNext(int value);
+
+        public abstract void StartSimulation(int numOfBalls);
+
+        public abstract IBall GetBallModel(int index);
+
+        public abstract int GetCanvasWidth();
+        public abstract int GetCanvasHeight();
+
+        public static ModelAbstractAPI CreaetModelAPI(LogicAbstractAPI logicAPI = default)
+        {
+            return new ModelAPI(logicAPI ?? LogicAbstractAPI.CreateLogicAPI());
+        }
+
+        public abstract IDisposable Subscribe(IObserver<IBall> observer);
+
+        private class ModelAPI : ModelAbstractAPI
+        {
+            private readonly LogicAbstractAPI logicAPI;
+            private readonly List<BallModel> Balls = new List<BallModel>();
+            private readonly IDisposable unsubscriber;
+            private IObserver<IBall> observer;
+
+            private class Unsubscriber : IDisposable
+            {
+                private IObserver<IBall> _observer;
+
+                public Unsubscriber(IObserver<IBall> observer)
+                {
+                    _observer = observer;
+                }
+
+                public void Dispose()
+                {
+                    _observer = null;
+                }
+            }
+
+            public ModelAPI(LogicAbstractAPI logicAPI)
+            {
+                System.Diagnostics.Trace.WriteLine("Initializing ModelAPI");
+
+                this.logicAPI = logicAPI;
+                unsubscriber = logicAPI.Subscribe(this);
+            }
+
+            public override IBall GetBallModel(int index)
+            {
+                return Balls[index];
+            }
+
+            public override void OnCompleted()
+            {
+                unsubscriber.Dispose();
+            }
+
+            public override void OnError(Exception error)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void OnNext(int value)
+            {
+                if (value < Balls.Count)
+                {
+                    BallModel ball = Balls[value];
+                    Vector2 position = logicAPI.GetBallPosition(value);
+                    float diameter = 2 * logicAPI.GetBallRadius(value) * (GetCanvasWidth() / logicAPI.GetTableWidth());
+                    double top = position.Y * GetCanvasHeight() / logicAPI.GetTableHeight() - (diameter / 2);
+                    double left = position.X * GetCanvasWidth() / logicAPI.GetTableWidth() - (diameter / 2);
+
+                    System.Diagnostics.Trace.WriteLine("ModelAPI: Updating Balls[" + value + "] position: (" + top + ", " + left + ")");
+                    ball.Move(top, left);
+                }
+            }
+
+            public override void StartSimulation(int numOfBalls)
+            {
+                System.Diagnostics.Trace.WriteLine("ModelAPI: Starting simulation with " + numOfBalls + " balls");
+
+                logicAPI.StartSimulation(numOfBalls);
+
+                for (int i = 0; i < numOfBalls; i++)
+                {
+                    Vector2 position = logicAPI.GetBallPosition(i);
+                    float diameter = 2 * logicAPI.GetBallRadius(i) * (GetCanvasWidth() / logicAPI.GetTableWidth());
+                    double top = position.Y * GetCanvasHeight() / logicAPI.GetTableHeight() - (diameter / 2);
+                    double left = position.X * GetCanvasWidth() / logicAPI.GetTableWidth() - (diameter / 2);
+
+                    System.Diagnostics.Trace.WriteLine("ModelAPI: Creating BallModel: Id = " + i + "; Position = (" + top + ", " + left + ")");
+                    BallModel ball = new BallModel(i, top, left, diameter);
+                    Balls.Add(ball);
+                }
+            }
+
+            public override IDisposable Subscribe(IObserver<IBall> observer)
+            {
+                this.observer = observer;
+                return new Unsubscriber(this.observer);
+            }
+
+            public override int GetCanvasWidth()
+            {
+                return 576; // 128 * 4.5
+            }
+
+            public override int GetCanvasHeight()
+            {
+                return 432; // 96 * 4.5
+            }
+        }
+    }
+}
